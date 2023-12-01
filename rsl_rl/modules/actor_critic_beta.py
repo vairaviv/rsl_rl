add actor critic recurrent with beta distr & fix bug in alpha beta computation

rsl_rl/modules/actor_critic_beta.py
@@ -105,9 +105,14 @@ class ActorCriticBeta(nn.Module):
         return self.distribution.entropy().sum(dim=-1)
     
     def get_beta_parameters(self, logits):
-        """Get alpha and beta parameters from logits"""
-        ratio = self.sigmoid(logits[:, :self.output_dim] + self.beta_initial_logit_shift) # alpha/(alpha+beta) --> Mean
-        sum = (self.soft_plus(logits[:, self.output_dim:]) + 1) * self.beta_initial_scale # alhpa+beta --> inverse variance
+        """Get alpha and beta parameters from logits
+        NOTE: usually logits has dimensions (batch_size, 2*output_dim), but when using recurrent architecture
+        we have after the rollout a tensor of shape (steps_per_env, 2, 2*output_dim)
+        """
+        ratio = self.sigmoid(logits[..., :self.output_dim] + self.beta_initial_logit_shift)
+        sum = (self.soft_plus(logits[..., self.output_dim:]) + 1) * self.beta_initial_scale
+
+        # Compute alpha and beta
         alpha = ratio * sum
         beta = sum - alpha
         return alpha, beta

rsl_rl/runners/on_policy_runner.py
@@ -13,7 +13,7 @@ from torch.utils.tensorboard import SummaryWriter as TensorboardSummaryWriter
 import rsl_rl
 from rsl_rl.algorithms import PPO
 from rsl_rl.env import VecEnv
-from rsl_rl.modules import ActorCritic, ActorCriticRecurrent, ActorCriticBeta, EmpiricalNormalization
+from rsl_rl.modules import ActorCritic, ActorCriticRecurrent, ActorCriticBeta, ActorCriticRecurrentBeta, EmpiricalNormalization
 from rsl_rl.utils import store_code_state
 
 
@@ -32,8 +32,8 @@ class OnPolicyRunner:
             num_critic_obs = extras["observations"]["critic"].shape[1]
         else:
             num_critic_obs = num_obs
-        actor_critic_class = eval(self.policy_cfg.pop("class_name"))  # ActorCritic | ActorCriticRecurrent | ActorCriticBeta
-        actor_critic: ActorCritic | ActorCriticRecurrent | ActorCriticBeta = actor_critic_class(
+        actor_critic_class = eval(self.policy_cfg.pop("class_name"))  # ActorCritic | ActorCriticRecurrent | ActorCriticBeta | ActorCriticRecurrentBeta
+        actor_critic: ActorCritic | ActorCriticRecurrent | ActorCriticBeta | ActorCriticRecurrentBeta= actor_critic_class(
             num_obs, num_critic_obs, self.env.num_actions, **self.policy_cfg
         ).to(self.device)
         alg_class = eval(self.alg_cfg.pop("class_name"))  # PPO